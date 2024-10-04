1fe6897857bd664be4717a90a1fe2525eef457b5
Will Moran <will.c.moran@gmail.com>
2024-10-04 15:18:59 -0400

    finished houses v1 backend

--- FieldScout/Program.cs
+++ FieldScout/Program.cs
@@ -14,6 +14,7 @@ namespace FieldScout
             builder.Services.AddControllers();
             builder.Services.AddTransient<IUserProfileRepository, UserProfileRepository>();
             builder.Services.AddTransient<IFacilitiesRepository,  FacilitiesRepository>();
+            builder.Services.AddTransient<IHousesRepository, HousesRepository>();
             // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
             builder.Services.AddEndpointsApiExplorer();
             builder.Services.AddSwaggerGen();
---
+++ FieldScout/Repositories/IHousesRepository.cs
@@ -0,0 +1,14 @@
+﻿using FieldScout.Models;
+
+namespace FieldScout.Repositories
+{
+    public interface IHousesRepository
+    {
+        void Add(Houses house);
+        void Delete(int id);
+        List<Houses> Get();
+        Houses GetById(int id);
+        Houses GetByName(string name);
+        void Update(Houses house);
+    }
+}
