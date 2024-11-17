416abdaab008d30d244a7f45ebb3ba81989fce15
Will Moran <will.c.moran@gmail.com>
2024-11-17 09:06:12 -0500

    first

--- FieldScout/client/src/components/navbar/ApplicationNavbar.jsx
+++ FieldScout/client/src/components/navbar/ApplicationNavbar.jsx
@@ -5,54 +5,55 @@ import {
   Nav,
   NavItem,
   NavLink,
-  NavbarText
+  NavbarText,
+  Collapse,
+  NavbarToggler
 } from 'reactstrap';
 import './NavBar.css'
 import { useNavigate } from 'react-router-dom';
 
 export const ApplicationNavbar = () => {
     const navigate = useNavigate()
+    const [isOpen, setIsOpen] = useState(false)
+
+    const toggle = () => setIsOpen(!isOpen)
     
     return (
         <div>
         <Navbar className="navFix" container='fluid'>
             <NavbarBrand href="/">Field Scout</NavbarBrand>
-            <Nav className="ml-auto">
-                <NavItem>
-                    <NavLink href="/greenhouseManagement">
-                        Greenhouse Management
-                    </NavLink>
-                </NavItem>
-                <NavItem>
-                    <NavLink href="/pests">
-                        Pest Management
-                    </NavLink>
-                </NavItem>
-                <NavItem>
-                    <NavLink href="/scouting">
-                        Scouting
-                    </NavLink>
-                </NavItem>
-                <NavItem>
-                    <NavLink href="/trends">
-                        Trends
-                    </NavLink>
-                </NavItem>
-                <NavItem>
-                    <NavLink href="/login" onClick={() => {
-                                                localStorage.removeItem("fieldScout_user")
-                    }}>
-                        Log Out
-                    </NavLink>
-                </NavItem>
-            </Nav>
-            {/* <NavbarText>
-                <button onClick={() => {
-                                localStorage.removeItem("fieldScout_user")
-                                navigate("/login")}}>
-                    Log Out
-                </button>
-            </NavbarText> */}
+            <NavbarToggler onClick={toggle} />
+            <Collapse isOpen={isOpen}>
+                <Nav className="ml-auto">
+                    <NavItem>
+                        <NavLink href="/greenhouseManagement">
+                            Greenhouse Management
+                        </NavLink>
+                    </NavItem>
+                    <NavItem>
+                        <NavLink href="/pests">
+                            Pest Management
+                        </NavLink>
+                    </NavItem>
+                    <NavItem>
+                        <NavLink href="/scouting">
+                            Scouting
+                        </NavLink>
+                    </NavItem>
+                    <NavItem>
+                        <NavLink href="/trends">
+                            Trends
+                        </NavLink>
+                    </NavItem>
+                    <NavItem>
+                        <NavLink href="/login" onClick={() => {
+                                                    localStorage.removeItem("fieldScout_user")
+                        }}>
+                            Log Out
+                        </NavLink>
+                    </NavItem>
+                </Nav>
+            </Collapse>
         </Navbar>
         </div>
     )
