9579ae57321f07b3a6657c4d115f28ef9fd4af3a
Will Moran <113952513+wicamo96@users.noreply.github.com>
2024-11-26 20:37:30 -0500

    Update 02_FieldScout_Seed_Data.sql

--- SQL/02_FieldScout_Seed_Data.sql
+++ SQL/02_FieldScout_Seed_Data.sql
@@ -3,31 +3,6 @@ INSERT INTO Facilities (name) VALUES
 ('Sunny Acres'),
 ('Maple Grove');
 
-INSERT INTO Houses (name) VALUES
-('House A'),
-('House B'),
-('House C');
-
-INSERT INTO FacilityHouses (facilityId, houseId) VALUES
-(1, 1),
-(1, 2),
-(2, 3);
-
-INSERT INTO Bays (name) VALUES
-('Bay 1'),
-('Bay 2'),
-('Bay 3');
-
-INSERT INTO HouseBays (bayId, houseId) VALUES
-(1, 1),
-(2, 2),
-(3, 3);
-
-INSERT INTO BayDivisions (name, bayId) VALUES
-('Division A', 1),
-('Division B', 1),
-('Division C', 2);
-
 INSERT INTO Pests (name) VALUES
 ('Aphids'),
 ('Fungus Gnats'),
@@ -39,8 +14,3 @@ INSERT INTO UserProfile (facilityId, name, email) VALUES
 (1, 'John Doe', 'john@example.com'),
 (2, 'Jane Smith', 'jane@example.com'),
 (1, 'Emily Johnson', 'emily@example.com');
-
-INSERT INTO ScoutingReport (userProfileId, pestId, pressure, bayDivisionId, date, facilityId, growingWeek) VALUES
-(1, 1, '1', 1, '2024-10-01 09:00:00', 1, 40),
-(2, 2, '3', 2, '2024-10-02 10:30:00', 1, 40),
-(3, 3, '2', 1, '2024-10-03 11:15:00', 1, 40);
